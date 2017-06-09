53dfefbe116c6ddfd9676ccfff7f73c1bb3c1bea
Blessing Philips <blessingadesina@gmail.com>
2017-06-09 13:23:58 +0100

    chore(backend): Setup Express Server
    [Finishes #146922531]

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./server/routes');
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+routes.Document(app);
+routes.User(app);
+routes.Role(app);
+module.exports = app;
