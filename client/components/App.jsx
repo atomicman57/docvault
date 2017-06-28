59da5322b6dcb3cc74c5e9ba3d2cf1f8eda2020e
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 21:50:54 +0100

    feature(frontend): Add loading spinner to the Frontend
     [Finishes #147997651]

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
 import NavigationBar from './includes/NavigationBar.jsx';
 import Footer from './includes/Footer.jsx';
+import Spinner from './includes/Spinner.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <NavigationBar />
+         {/*<Spinner />*/}
+         {(this.props.loading > 0) && <Spinner />}
         {this.props.children}
         <Footer />
       </div>
@@ -14,4 +19,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+   loading: state.ajaxCallsInProgress
+});
+export default connect(mapStateToProps)(App);
