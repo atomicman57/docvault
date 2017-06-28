5f440dad95363f0f132e758070b9dd535e26e7be
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 04:19:28 +0100

    feature(testing): Setup unit testing for the backend
    [Finishes #146987359]

--- client/components/documents/DocumentCard.jsx
+++ client/components/documents/DocumentCard.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Modal } from 'react-materialize';
 import renderHTML from 'react-render-html';
 import EditDocument from './EditDocument.jsx';
