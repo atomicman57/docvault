7d7145af1208d8cdeb61f6de79707e554080e5da
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 04:20:01 +0100

    feature(testing): Setup unit testing for the backend
    [Finishes #146987359]

--- client/components/users/MyProfilePage.jsx
+++ client/components/users/MyProfilePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import MyProfile from '../users/MyProfile.jsx';
 import { userUpdateUserRequest } from '../../actions/userActions';
 
