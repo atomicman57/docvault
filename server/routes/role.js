dd7673830c9aea45baa1a4acd3860531dc03542e
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 04:20:49 +0100

    feature(testing): Setup unit testing for the backend
    [Finishes #146987359]

--- server/routes/role.js
+++ server/routes/role.js
@@ -1,11 +1,32 @@
-const { Role } = require('../controllers');
+import { Role } from '../controllers';
+import Authentication from '../middleware/Authentication';
 
 const roleRoutes = (app) => {
-  app.post('/roles', Role.create);
-  app.get('/roles', Role.list);
-  app.get('/roles/:roleId', Role.find);
-  app.put('/roles/:roleId', Role.update);
-  app.delete('/roles/:roleId', Role.delete);
+  app.post(
+    '/roles',
+    Authentication.checkToken,
+    Authentication.allowAdmin,
+    Role.create
+  );
+  app.get('/roles', Authentication.checkToken, Role.list);
+  app.get(
+    '/roles/:roleId',
+    Authentication.checkToken,
+    Authentication.allowAdmin,
+    Role.find
+  );
+  app.put(
+    '/roles/:roleId',
+    Authentication.checkToken,
+    Authentication.allowAdmin,
+    Role.update
+  );
+  app.delete(
+    '/roles/:roleId',
+    Authentication.checkToken,
+    Authentication.allowAdmin,
+    Role.delete
+  );
 };
 
 export default roleRoutes;
