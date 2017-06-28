317b032b66dc29ecf1e070f7c7d5fe44eece1c3b
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 04:20:24 +0100

    feature(testing): Setup unit testing for the backend
    [Finishes #146987359]

--- server/middleware/Authentication.js
+++ server/middleware/Authentication.js
@@ -40,39 +40,15 @@ class Middleware {
    * @param {any} next
    * @memberof Middleware
    */
-  static checkAdmin(req, res, next) {
-    User.findById(req.decoded.id).then((user) => {
-      Role.findById(user.roleId).then((role) => {
-        if (role.title === 'Admin') {
-          next();
-        } else {
-          return res.status(403).json({
-            message: 'Sorry, You do not have sufficient permission'
-          });
-        }
-      });
-    });
-  }
-
-  /**
-   *
-   *
-   * @static
-   * @param {any} req
-   * @param {any} res
-   * @param {any} next
-   * @returns
-   * @memberof Middleware
-   */
-  static allowUserOrAdminn(req, res, next) {
-    return User.findById(req.params.id).then((user) => {
-      if (!user) return res.status(404).send({ message: 'No User Found' });
-      if (res.decoded.roleId !== 2 && res.decoded.id !== user.id) {
-        return res.status(403).send({ message: 'You do not have access' });
+  static allowAdmin(req, res, next) {
+    Role.findById(req.decoded.roleId).then((role) => {
+      if (role.title === 'Admin') {
+        next();
+      } else {
+        return res.status(403).json({
+          message: 'Sorry, You do not have sufficient permission'
+        });
       }
-
-      res.user = user;
-      return next();
     });
   }
 
@@ -87,32 +63,74 @@ class Middleware {
    * @memberof Middleware
    */
   static allowUserOrAdmin(req, res, next) {
-    return Document.findById(req.params.documentId).then((document) => {
-      if (!document) {
-        return res.status(404).json({ message: 'Document not found' });
-      }
+    return User.findById(req.params.id)
+      .then((user) => {
+        if (!user) return res.status(404).send({ message: 'No User Found' });
+        if (req.decoded.roleId !== 2 && req.decoded.id !== user.id) {
+          return res.status(403).send({ message: 'You do not have access' });
+        }
 
-      if (
-        req.decoded.roleId === 2 &&
-        document.userId !== req.decoded.id &&
-        document.access === 'private'
-      ) {
-        return res
-          .status(403)
-          .json({ message: 'You do not have access to private documents' });
-      }
-
-      if (req.decoded.roleId === 2) {
+        res.user = user;
         return next();
-      }
+      })
+      .catch(error => res.status(400).json(error));
+  }
 
-      if (req.decoded.id !== document.userId) {
-        return res.status(403).send({ message: 'You do not have access' });
-      }
+  /**
+   *
+   *
+   * @static
+   * @param {any} req
+   * @param {any} res
+   * @param {any} next
+   * @returns
+   * @memberof Middleware
+   */
+  static allowUserOrAdminDoc(req, res, next) {
+    return Document.findById(req.params.documentId)
+      .then((document) => {
+        if (!document) {
+          return res.status(404).json({ message: 'Document not found' });
+        }
 
-      res.document = document;
-      return next();
-    });
+        if (
+          document.access === 'role' &&
+          document.userRoleId == req.decoded.roleId
+        ) {
+          return next();
+        }
+
+        if (
+          req.decoded.roleId === 2 &&
+          document.userId !== req.decoded.id &&
+          document.access === 'private'
+        ) {
+          return res
+            .status(403)
+            .json({ message: 'You do not have access to private documents' });
+        }
+
+        if (req.decoded.roleId === 2) {
+          return next();
+        }
+
+        if (req.decoded.id !== document.userId) {
+          return res.status(403).send({ message: 'You do not have access' });
+        }
+        return next();
+      })
+      .catch(error => res.status(400).json(error));
+  }
+
+  static allowUser(req, res, next) {
+    return Document.findById(req.params.documentId)
+      .then((document) => {
+        if (!document) {
+          return res.status(404).json({ message: 'Document not found' });
+        }
+        return next();
+      })
+      .catch(error => res.status(400).json(error));
   }
 }
 export default Middleware;
