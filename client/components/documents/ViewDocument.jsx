8994444e473ab808fe87d776d1d27da67fb9d5d9
Blessing Philips <blessingadesina@gmail.com>
2017-06-28 04:19:48 +0100

    feature(testing): Setup unit testing for the backend
    [Finishes #146987359]

--- client/components/documents/ViewDocument.jsx
+++ client/components/documents/ViewDocument.jsx
@@ -15,7 +15,7 @@ class ViewDocument extends React.Component {
             <p className="meta-info">
               Created on: {new Date(document.createdAt).toDateString()},
               by:
-              <span className="pink-text darken-4">{document.User.username}</span>
+              <span className="pink-text darken-4"> {document.User.username}</span>
             </p>
             <div>{renderHTML(document.content)} </div>
           </div>
